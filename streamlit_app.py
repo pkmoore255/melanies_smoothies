e9cd44b0524ed8f64827f9b3b7ae9fc7b067df9a
pkmoore255 <pk.conder@yahoo.com>
2025-01-03 16:34:09 -0500

    Change nutrition format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -51,6 +52,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 # Display smoothiefroot nutrition information
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
